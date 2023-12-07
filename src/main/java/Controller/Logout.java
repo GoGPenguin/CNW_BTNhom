678894e86fb5bf9da9e2b827e12c842979b7c0af
unknown <nguyenthuchoang17112003@gmail.com>
2023-12-07 11:03:16 +0700

    fix bugs

--- src/main/java/Controller/Logout.java
+++ src/main/java/Controller/Logout.java
@@ -1,4 +1,16 @@
 package Controller;
 
-public class Logout {
+import Model.BEAN.User;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class Logout{
 }
